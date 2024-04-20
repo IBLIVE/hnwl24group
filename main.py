73a1309890d7b23be055762978cabe52ba70c273
alexandsomeadventures <educationfor300@gmail.com>
2024-04-20 16:22:42 -0700

    added image of each challenge

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 challenges_list = [
-    {'id': 1, 'title': 'Plane Landing', 'description': 'Solve the first problem.', 'level': "Easy", 'reward': 10},
-    {'id': 2, 'title': 'Plane maneuvering', 'description': 'Solve the second problem.', 'level': "Medium", 'reward': 50},
-    {'id': 3, 'title': 'Plane in bad weather', 'description': 'Solve the third problem.', 'level': "Hard", 'reward': 100}
+    {'id': 1, 'title': 'Plane Landing', 'description': 'Solve the first problem.', 'level': "Easy", 'reward': 10, "imageurl": "plane_landing.png"},
+    {'id': 2, 'title': 'Plane maneuvering', 'description': 'Solve the second problem.', 'level': "Medium", 'reward': 50, "imageurl": "plane_maneuvering.png"},
+    {'id': 3, 'title': 'Plane in bad weather', 'description': 'Solve the third problem.', 'level': "Hard", 'reward': 100, "imageurl": "plane_weather.png"}
 ]
 
 @app.route("/")
