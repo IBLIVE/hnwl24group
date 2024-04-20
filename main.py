16a5820902089725a4a94e12c38c6ac60b2172ca
alexandsomeadventures <educationfor300@gmail.com>
2024-04-20 15:04:00 -0700

    added challenges list

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+challenges_list = [
+    {'id': 1, 'title': 'Plane Landing', 'description': 'Solve the first problem.', 'level': "Easy", 'reward': 10},
+    {'id': 2, 'title': 'Plane maneuvering', 'description': 'Solve the second problem.', 'level': "Medium", 'reward': 50},
+    {'id': 3, 'title': 'Plane in bad weather', 'description': 'Solve the third problem.', 'level': "Hard", 'reward': 100}
+]
 
 @app.route("/")
 def main_page():
@@ -10,7 +15,7 @@ def main_page():
 
 @app.route("/challenges")
 def challenges():
-    return render_template("challenges.html")
+    return render_template("challenges.html", challenges = challenges_list)
 
 
 @app.route("/forums")
