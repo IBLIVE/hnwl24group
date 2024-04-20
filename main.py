956673102cf35d0482448f8b0420bc3576436dde
ikushbay <ikushbay@asu.edu>
2024-04-20 14:03:54 -0600

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    return "main page"
+    return render_template("base.html")
+
+
+@app.route("/challenges")
+def challenges():
+    return render_template("challenges.html")
 
 
 if __name__ == "__main__":
