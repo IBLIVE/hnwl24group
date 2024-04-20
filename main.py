8745320b8b91703f38d693d5dceafbe5f06d68d1
alexandsomeadventures <educationfor300@gmail.com>
2024-04-20 12:37:09 -0700

    added new routes

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ def main_page():
     return "main page"
 
 
+@app.route("/challenges")
+def challenges_page():
+    return "placeholder for challenges"
+
+
+@app.route("/forums")
+def forums_page():
+    return "placeholder for forums"
+
 if __name__ == "__main__":
     app.run(debug=True)
